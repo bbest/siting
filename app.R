dbff76d66e6d2c76108e074850a40b7887191d02
Ben Best <ben@ecoquants.com>
2019-03-25 15:26:53 -0700

    added NaturalEarth state boundaries for Plos One copyright compliance

--- app.R
+++ app.R
@@ -5,11 +5,13 @@
 suppressPackageStartupMessages({
   library(shiny)
   library(raster)
+  library(sf)
   library(dplyr)
   library(leaflet)
   library(readr)
   library(ggplot2)
   library(plotly)
+  library(rnaturalearth)
   library(RColorBrewer)
   library(DT)
 })
@@ -75,6 +77,56 @@ if (file.exists('~/github/consmap-prep') & redo_rasters){
   s_c = stack('data/cetaceans/spp_EC_nzw_aea_ALL_nfzw_1to12_scaled0to1.grd')
 }
   
+
+# get USA states
+us = rnaturalearth::ne_states(iso_a2 = "US", returnclass = "sf") %>% 
+  mutate(
+    ctr = st_centroid(geometry),
+    lon = st_coordinates(ctr)[,1],
+    lat = st_coordinates(ctr)[,2],
+    code = c(setNames(state.abb, state.name), 'District of Columbia'='DC')[name])
+
+# st_coordinates(us$ctr)
+# # get East Coast states
+# ec = subset(us,
+#             code %in% 
+#               c('ME','VT', # NE
+#                 'NY','MA','RI','CT','PA','NJ','MD','DE','DC','WV','VA','NC', # Mid-Atlantic
+#                 'SC','GA','FL')) # South
+# ec@data %>%
+#   arrange(desc(pt_lat)) %>%
+#   .$code %>% paste0("','") %>% cat(sep='')
+# get Mid-Atlantic states
+# ma_gcs = subset(us,
+#                 code %in% 
+#                   c('NY','MA','RI','CT','PA','NJ','MD','DE','DC','WV','VA','NC')) # Mid-Atlantic
+# # project to aea
+# ma = spTransform(ma_gcs, crs(r_b))
+#ma_gadm <- ma
+#plot(ma_gadm)
+#plot(ma)
+
+# get plotting coordinates
+# ma@data = ma@data %>%
+#   mutate(
+#     x = coordinates(ma)[,1],
+#     y = coordinates(ma)[,2])
+# ma_pts_manual = list( # get from plotting raster and using click()
+#   NY=c(318279.0, 861861.0),
+#   MA=c(583509.0, 900670.0),
+#   PA=c(176331.0, 753784.4),
+#   NJ=c(302205.0, 697670.0),
+#   DE=c(218600.4, 532496.3),
+#   MD=c(116658.7, 604601.4),
+#   VA=c(87289.93, 403240.4), #58407.0, 403619.0),
+#   NC=c(142765.7, 194348.2))
+# for (code in names(ma_pts_manual)){
+#   xy = ma_pts_manual[[code]]
+#   ma@data$x[ma@data$code==code] = xy[1]
+#   ma@data$y[ma@data$code==code] = xy[2]
+# }
+
+
 # ui: user interface ----
 ui <- fluidPage(
   navbarPage(title='Conservation',
@@ -138,22 +190,100 @@ server <- function(input, output, session){
   
   # output$map ----
   output$map = renderLeaflet({
+
+    # get USA states
+    # us = rnaturalearth::ne_states(iso_a2 = "US", returnclass = "sf") %>% 
+      # mutate(
+      #   ctr = st_centroid(geometry, of_largest_polygon = T),
+      #   lon = st_coordinates(ctr)[,1],
+      #   lat = st_coordinates(ctr)[,2],
+      #   code = c(setNames(state.abb, state.name), 'District of Columbia'='DC')[name])
+      # filter(code != "DC") %>% 
+      # left_join(
+      #   tribble( # get from plotting raster and using click()
+      #     ~code, ~lon, ~lat
+      #     "NY", 318279.0, 861861.0),
+      #     "MA", 583509.0, 900670.0),
+      #     "PA", 176331.0, 753784.4),
+      #     "NJ", 302205.0, 697670.0),
+      #     "DE", 218600.4, 532496.3),
+      #     "MD", 116658.7, 604601.4),
+      #     "VA", 87289.93, 403240.4), #58407.0, 403619.0),
+      #     "NC", 142765.7, 194348.2))
+      #   
+        
+      #)
+      
+      #mapedit::editFeatures()    
+
+      # tribble( # get from plotting raster and using click()
+      # code, lon, lat
+      # NY=c(318279.0, 861861.0),
+      # MA=c(583509.0, 900670.0),
+      # PA=c(176331.0, 753784.4),
+      # NJ=c(302205.0, 697670.0),
+      # DE=c(218600.4, 532496.3),
+      # MD=c(116658.7, 604601.4),
+      # VA=c(87289.93, 403240.4), #58407.0, 403619.0),
+      # NC=c(142765.7, 194348.2))
     
+    # m <- 
+    # us %>%
+    #   filter(
+    #     code %in% c('NY','MA','RI','CT','PA','NJ','MD','DE','WV','VA','NC')) %>% # Mid-Atlantic
+    #   leaflet() %>%
+    #   #addTiles() %>%
+    #   addPolygons(
+    #     color = "#4D4D4D", fill = FALSE, weight=2, opacity=1) %>%
+    #   addLabelOnlyMarkers(
+    #     lng = ~lon, lat = ~lat, label = ~code,
+    #     labelOptions = labelOptions(
+    #       noHide = T, direction = 'top', textOnly = T)) #%>%
+    #   mapedit::editMap()
+    # data = us, stroke = T, color = "gray80", fill=F) %>%
+    #fitBounds()
+    # gplots::col2hex("gray90") # "#E5E5E5"
+    # gplots::col2hex("gray10") # "#1A1A1A"
+    # gplots::col2hex("gray30") # "#1A1A1A"
+      
+    bb <- projectExtent(r_u, crs=leaflet:::epsg4326) %>% extent() %>% as.vector()
+        
+    # leaflet(data = quakes[1:20,]) %>% addTiles() %>%
+    #   addLabelOnlyMarkers(~long, ~lat, label =  ~as.character(mag), 
+    #                       labelOptions = labelOptions(noHide = T, direction = 'top', textOnly = T))
+    # 
+    
+   us %>%
+      filter(
+        !code %in% c("HI", "AK", "DC")) %>% 
     leaflet() %>%
-      addProviderTiles('Stamen.TonerLite') %>%
-      addRasterImage(r_u    , colors=pal, opacity=0.3, layerId='r_u') %>%
-      addRasterImage(r_u, colors=pal, opacity=1  , layerId='r_sel') %>%
+      addProviderTiles(providers$Esri.OceanBasemap, group="Esri ocean") %>%
+      addProviderTiles(providers$Stamen.TonerLite, group="Stamen b&w") %>%
+      addPolygons(
+        group="States",
+        color = "#4D4D4D", fill = FALSE, weight=2, opacity=1) %>% 
+      addLabelOnlyMarkers(
+        lng = ~lon, lat = ~lat, label = ~code, group="States",
+        labelOptions = labelOptions(
+          noHide = T, direction = 'top', textOnly = T)) %>% 
+      addLayersControl(
+        baseGroups    = c("Stamen b&w", "Esri ocean", "None"),
+        overlayGroups = c("States", "Utility")) %>% 
+      addRasterImage(r_u    , colors=pal, opacity=0.3, layerId='r_u', group="Utility") %>%
+      addRasterImage(r_u, colors=pal, opacity=1  , layerId='r_sel', group="Utility") %>%
       addLegend('bottomright', pal=pal_rev, values=values(r_u),
-                title = 'u', opacity = 1,
-                labFormat = labelFormat(transform = function(x) rev(x)))
-    
+                title = 'Utility', opacity = 1,
+                labFormat = labelFormat(transform = function(x) rev(x))) %>% 
+     hideGroup("States") %>% 
+     fitBounds(bb[1], bb[3], bb[2], bb[4])
+   
   })
   
   # draw opaque utility raster of selected sites in tradeoff plot
   observe({
     leafletProxy('map') %>%
       removeImage('r_sel') %>%
-      addRasterImage(r_sel(), colors=pal, opacity=1  , layerId='r_sel')
+      addRasterImage(r_sel(), colors=pal, opacity=1, layerId='r_sel', group="Utility")
   })
   
   # add popup on map of site values
@@ -200,8 +330,11 @@ server <- function(input, output, session){
     if(is.null(d)){
       d_sum
     } else {
+      #browser()
       d_sum %>%
-        semi_join(d, by='key')
+        semi_join(
+          d %>% 
+            mutate(key = as.integer(key)), by='key')
     }
   })
   
